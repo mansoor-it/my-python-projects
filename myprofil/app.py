e83628e8142b37f252d1e70f73405c4061129949
mansoor-it <acomputermansoor485@gmail.com>
2025-08-01 23:24:56 +0300

    Add files via upload

---
+++ myprofil/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('contact.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
